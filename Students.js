d8e2a96f80a7eeb53130cb5599add32060493695
Masterofai1 <bantigupta33552@gmail.com>
2026-09-11 17:26:10 +0530

    Create Students.js

---
+++ Students.js
@@ -0,0 +1,16 @@
+const students = [
+  {
+    name:"harsh",
+    father:"ghanshyam mishra",
+    nameDisplay:"Harsh",
+    fatherDisplay:"Ghanshyam Mishra",
+    className:"11th",
+    batch:"122",
+    id:"2199020937",
+    timing:"12:10 – 02:10",
+    status:"ACTIVE",
+    role:"STUDENT"
+  },
+
+  // ...baaki 200 existing students...
+];
